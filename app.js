e130986c502794fb1723984505a63f87f3762832
Edmond Hui <edmond.t.hui@gmail.com>
2020-12-19 14:31:28 -0500

    :sparkles: finish backend user auth

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI
 const bodyParser = require('body-parser');
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const options = require("./routes/api/options");
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
