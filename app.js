33aad6334074eac35afce7492964fa15ca26f406
Edmond Hui <edmond.t.hui@gmail.com>
2020-12-19 00:08:00 -0500

    :tada: Initial commit for new MERN project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI
+const bodyParser = require('body-parser');
+
+const users = require("./routes/api/users");
+const options = require("./routes/api/options");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err))
+
+
+app.use("/api/users", users);
+app.use("/api/options", options);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
