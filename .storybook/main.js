bff49627c620308e36ce38bacb9e99d9f48e4243
Andrew Hu <andrew.hu@pithecine.com>
2020-12-19 18:28:03 -0500

    setup: ✨ CRA Typescript + Storybook + directory structure

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../web/src/**/*.stories.mdx",
+    "../web/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
