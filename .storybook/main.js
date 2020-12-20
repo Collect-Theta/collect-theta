be26b66134786083eac70c1a3fe56a5f96a29c64
Andrew Hu <andrew.hu@pithecine.com>
2020-12-19 19:44:50 -0500

    update(storybook): 📚 Add default stories as examples

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
   "stories": [
+    "../examples/stories/**/*.stories.mdx",
+    "../examples/stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../web/src/**/*.stories.mdx",
     "../web/src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
   ]
 }
