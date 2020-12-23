89a5ecb45272462f2d59e7564417b520f0026603
Edmond Hui <edmond.t.hui@gmail.com>
2020-12-23 12:04:22 -0500

    Index  - start on user auth (#16)
    * change npm to yarn
    
    * update package.json
    
    * update package.json
    
    * update package.json
    
    * update package.json
    
    * change yarn to npm
    
    * :poop: add frontend user auth with jwt token + create store
    
    * :adhesive_bandage: update file names

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,14 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store/store';
 import reportWebVitals from './reportWebVitals';
+import { setAuthToken } from './util/sessionApiUtil';
+import jwt_decode from 'jwt-decode';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  let store; 
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const user: any = jwt_decode(localStorage.jwtToken);
+    const preloadedState = {
+      session: {
+        isAuthenticated: true, 
+        user 
+      }
+    }
+    store = configureStore(preloadedState);
+    const currentTime = Date.now() / 1000;
+    if (user.exp < currentTime) {
+      // store.dispatch(logout()); // to do logout action
+    } else {
+      store = configureStore({});
+    }
+  }
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
